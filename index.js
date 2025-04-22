6c431958f309a6559901d1487413b607c67bfd2c
popkidk <michalkathara@gmail.com>
2025-04-22 14:27:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const adams = require("./config");
 
 async function fetchINDEXUrl() {
   try {
-    const response = await axios.get(adams.BWM_XMD);
+    const response = await axios.get(michal.MRLIKE);
     const $ = cheerio.load(response.data);
 
     const targetElement = $('a:contains("INDEX")');
